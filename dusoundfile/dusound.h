d6a344d3cbfc2033ae99b92aacd644f6a7415d8c
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2015-06-26 09:26:33 +0200

    update s_instr_m3

--- dusoundfile/dusound.h
+++ dusoundfile/dusound.h
@@ -7,33 +7,38 @@
 //TODO: this will be in an external
 #pragma pack(push, 4)
 
-#define NAME_CARACT	12
-#define INSTR_INFO_DUMMY 7
+#define NAME_CARACT    12
+#define INSTR_INFO_DUMMY 12
 
 typedef struct
 {
-    uint32_t KW_MMRY;
     uint8_t instr_name[NAME_CARACT];
+
     uint8_t instr_midi_pc;
     uint8_t instr_midi_C0;
     uint8_t instr_key_map;
     uint8_t instr_octave;
+
     uint32_t instr_user_id;
+
     uint32_t instr_id;
+
     uint32_t sample_address;
+
     uint8_t instr_noteoff;
     uint8_t instr_cat[NAME_CARACT];
+
     uint8_t instr_relvolume;
     uint8_t format_id;
-    uint8_t touche;
     uint8_t nb_layer;
     uint16_t ip_size;
     uint16_t sp_size;
     uint32_t sample_size;
+
     uint8_t instr_dummy[INSTR_INFO_DUMMY];
 } s_instr_m3;
 
-#define INSTR_INFO_INSTR_SIZE 64 //(NAME_CARACT*2 + 5*4 + 2*2 + 9*1 + INSTR_INFO_DUMMY)
+#define INSTR_INFO_INSTR_SIZE 64 //(NAME_CARACT*2 + 4*4 + 2*2 + 8*1 + INSTR_INFO_DUMMY)
 
 #pragma pack(pop)
 
