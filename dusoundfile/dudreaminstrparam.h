c1b688edd10bf89b1807929ef976632770114c4e
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2015-08-19 16:59:33 +0200

    Move dream_ip and dream_sp structs in parameters/sound_parameters_mng.h

--- dusoundfile/dudreaminstrparam.h
+++ dusoundfile/dudreaminstrparam.h
@@ -4,21 +4,6 @@
 #include "../general/ducontainer.h"
 
 
-#pragma pack(push, 4)
-struct dream_ip
-{
-    uint8_t min_vel;
-    uint8_t start_note;
-    uint8_t max_vel;
-    uint8_t end_note;
-
-    uint16_t dream_sp_addr;
-};
-
-#define INSTR_DREAM_IP_SIZE 6
-#pragma pack(pop)
-
-
 DU_OBJECT(DuDreamInstrParam)
 
 class DuDreamInstrParam : public DuContainer
