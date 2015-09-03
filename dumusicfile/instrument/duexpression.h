6c3c580842284fcad809387e32d12f1ce5aa1b73
Jean-François Coeur <jean-francois.coeur@dualo.org>
2015-09-03 13:22:38 +0200

    Fixed missing parameters

--- dumusicfile/instrument/duexpression.h
+++ dumusicfile/instrument/duexpression.h
@@ -5,22 +5,6 @@
 #include "../../general/dunumeric.h"
 
 
-#define KEY_EXPR_VOLUME                   "Volume"
-#define KEY_EXPR_PANNING                  "Panning"
-#define KEY_EXPR_OCTAVE                   "Octave"
-#define KEY_EXPR_SENDTOREVERB             "SendToReverb"
-
-#define KEY_EXPR_PORTAMENTOONOFF          "PortamentoOnOff"
-#define KEY_EXPR_PORTAMENTOCONTROL        "PortamentoControl"
-#define KEY_EXPR_PORTAMENTOTIME           "PortamentoTime"
-#define KEY_EXPR_EXPRESSION               "Expression"
-#define KEY_EXPR_PITCHBENDSENSITIVITY     "PitchBendSensitivity"
-#define KEY_EXPR_DISPOSITION              "Disposition"
-
-#define KEY_EXPR_ARPEGIATORTYPE           "ArpeggiatorType"
-#define KEY_EXPR_ARPEGIATORBEAT           "ArpeggiatorBeat"
-
-
 #define EXPRESSION_SIZE             12 + 8 + 1
 #define EXPRESSION_PRESET_OFFSET    0
 
@@ -42,41 +26,29 @@ public:
 
     int size() const;
 
-    int getVolume() const;
-    bool setVolume(int value);
+    DU_KEY_ACCESSORS(Volume,                int)
+    DU_KEY_ACCESSORS(Panning,               int)
+    DU_KEY_ACCESSORS(Octave,                int)
+    DU_KEY_ACCESSORS(SendToReverb,          int)
 
-    int getPanning() const;
-    bool setPanning(int value);
+    DU_KEY_ACCESSORS(PortamentoOnOff,       int)
+    DU_KEY_ACCESSORS(PortamentoControl,     int)
+    DU_KEY_ACCESSORS(PortamentoTime,        int)
+    DU_KEY_ACCESSORS(Expression,            int)
+    DU_KEY_ACCESSORS(PitchBendSensitivity,  int)
+    DU_KEY_ACCESSORS(Disposition,           int)
 
-    int getSendToReverb() const;
-    bool setSendToReverb(int value);
+    DU_KEY_ACCESSORS(AdsrPreset,            int)
+    DU_KEY_ACCESSORS(CompressorPreset,      int)
+    DU_KEY_ACCESSORS(DelayPreset,           int)
+    DU_KEY_ACCESSORS(DistortionPreset,      int)
+    DU_KEY_ACCESSORS(EqualizerPreset,       int)
+    DU_KEY_ACCESSORS(ChorusPreset,          int)
+    DU_KEY_ACCESSORS(VibratoPreset,         int)
+    DU_KEY_ACCESSORS(WahPreset,             int)
 
-    int getOctave() const;
-    bool setOctave(int value);
-
-    int getPortamentoOnOff() const;
-    bool setPortamentoOnOff(int value);
-
-    int getPortamentoControl() const;
-    bool setPortamentoControl(int value);
-
-    int getPortamentoTime() const;
-    bool setPortamentoTime(int value);
-
-    int getExpression() const;
-    bool setExpression(int value);
-
-    int getPitchBendSensitivity() const;
-    bool setPitchBendSensitivity(int value);
-
-    int getDisposition() const;
-    bool setDisposition(int value);
-
-    int getArpeggiatorType() const;
-    bool setArpeggiatorType(int value);
-
-    int getArpeggiatorBeat() const;
-    bool setArpeggiatorBeat(int value);
+    DU_KEY_ACCESSORS(ArpeggiatorType,       int)
+    DU_KEY_ACCESSORS(ArpeggiatorBeat,       int)
 };
 
 #endif // DUEXPRESSION_H
