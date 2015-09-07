24715a3333adb7fa1e918aaf7eb6abd7e610b742
Jean-François Coeur <jean-francois.coeur@dualo.org>
2015-09-07 16:46:40 +0200

    Update: no longer skips binary midi track size when parsing an uses it to check on the number of bytes parsed for one track

--- midifile/dumiditrack.cpp
+++ midifile/dumiditrack.cpp
@@ -30,8 +30,9 @@ DuMidiTrackPtr DuMidiTrack::fromMidiBinary(QDataStream &stream)
 
     if (trackId != MIDI_TRACK_ID_VALUE)
     {
-        qCCritical(LOG_CAT_DU_OBJECT) << "DuMidiTrack::fromMidiBinary():\n"
-                    << "the processed chunk is not a track";
+        qCCritical(LOG_CAT_DU_OBJECT)
+               << "DuMidiTrack::fromMidiBinary():\n"
+               << "the processed chunk is not a track";
 
         return DuMidiTrackPtr();
     }
@@ -43,9 +44,12 @@ DuMidiTrackPtr DuMidiTrack::fromMidiBinary(QDataStream &stream)
     quint32 offset = 0;
     quint32 delta = 0;
 
-    stream.skipRawData(MIDI_TRACK_SIZE_SIZE);
+    quint32 byteCount;
+    stream >> byteCount;
 
-    while (!trackEnded)
+    quint32 totalSize = 0;
+
+    while ((!trackEnded) && totalSize < byteCount)
     {
         DuMidiBasicEventPtr event(new DuMidiBasicEvent());
 
@@ -71,10 +75,17 @@ DuMidiTrackPtr DuMidiTrack::fromMidiBinary(QDataStream &stream)
                 {
                     event = DuMidiMetaEvent::fromMidiBinary(stream, &trackEnded);
                 }
-
-                else
+                else if (tmp == 0xF0 || tmp == 0xF7)
                 {
                     event = DuMidiSysExEvent::fromMidiBinary(stream, tmp);
+                }
+                else
+                {
+                    qCCritical(LOG_CAT_DU_OBJECT)
+                            << "DuMidiTrack::fromMidiBinary():\n"
+                            << "invalid status byte encountered" << tmp;
+
+                    return DuMidiTrackPtr();
                 }
             }
         }
@@ -85,8 +96,9 @@ DuMidiTrackPtr DuMidiTrack::fromMidiBinary(QDataStream &stream)
 
         if (event == NULL)
         {
-            qCCritical(LOG_CAT_DU_OBJECT) << "DuMidiTrack::fromMidiBinary():\n"
-                        << "problem encountered during event generation";
+            qCCritical(LOG_CAT_DU_OBJECT)
+                    << "DuMidiTrack::fromMidiBinary():\n"
+                    << "problem encountered during event generation";
 
             return DuMidiTrackPtr();
         }
@@ -98,6 +110,17 @@ DuMidiTrackPtr DuMidiTrack::fromMidiBinary(QDataStream &stream)
 
         if (trackEnded)
             track->m_duration = offset;
+
+        totalSize += event->size();
+    }
+
+    if (totalSize > byteCount)
+    {
+        qCCritical(LOG_CAT_DU_OBJECT)
+                << "DuMidiTrack::fromMidiBinary():\n"
+                << "total track size too big" << byteCount << totalSize;
+
+        return DuMidiTrackPtr();
     }
 
     return track;
