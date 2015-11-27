deb9d4b54a0249bcc74647f2f1d12e8ee1a0aa3e
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2015-11-27 17:13:20 +0100

    Add listToVariantList and listFromVariantList util functions

--- Util.h
+++ Util.h
@@ -2,10 +2,32 @@
 #define UTIL_H
 
 #include <QString>
+#include <QVariantList>
+#include <QUrl>
 
 namespace Util
 {
-QString sizeToString(int size);
+    QString sizeToString(int size);
+
+    template <typename T>
+    QVariantList listToVariantList(const QList<T> &list)
+    {
+        QVariantList newList;
+        foreach(const T &item, list)
+            newList << QVariant::fromValue(item);
+
+        return newList;
+    }
+
+    template <typename T>
+    QList<T> listFromVariantList(const QVariantList &list)
+    {
+        QList<T> newList;
+        foreach(const QVariant &item, list)
+            newList << item.value<T>();
+
+        return newList;
+    }
 }
 
 #endif // UTIL_H
