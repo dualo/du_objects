3d2976d35caa101ba376cafa1a1ae40defb1826c
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2016-03-22 17:15:22 +0100

    Remove unused parameter SaveLoopTimer

--- dumusicfile/duloop.h
+++ dumusicfile/duloop.h
@@ -35,8 +35,6 @@ DU_KEY_ACCESSORS(DurationModifier, int)
 DU_KEY_ACCESSORS(ScoreDisplay,     int)
 DU_KEY_ACCESSORS(MidiOutChannel,   int)
 
-DU_KEY_ACCESSORS(SaveLoopTimer,    int)
-
 DU_KEY_ACCESSORS_OBJECT(Instrument, DuMusicInstrument)
 DU_KEY_ACCESSORS_OBJECT(Events,     DuArray)
 
