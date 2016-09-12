9e4cd0603a03a700ea3552159302d19a56e2c502
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2016-09-12 17:14:45 +0200

    Fix default values of DuString (should not be a null QString, but an empty QString)

--- general/dustring.cpp
+++ general/dustring.cpp
@@ -14,7 +14,7 @@ DuString::DuString(const QString &value, int maxSize) :
 DuString::DuString(int maxSize) :
     DuValue(maxSize)
 {
-    setString(QString());
+    setString("");
 }
 
 DuObjectPtr DuString::clone() const
