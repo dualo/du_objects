d8695585670207179ab46295af8d0c42d14cd001
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2017-02-21 15:28:22 +0100

    Fix "Xo" in "XB" for size strings in english

--- Util.cpp
+++ Util.cpp
@@ -10,18 +10,18 @@ QString Util::sizeToString(int size)
     }
     else if (size < 1000)
     {
-        return QObject::tr("%1 bytes").arg(size);
+        return QObject::tr("%1 Bytes").arg(size);
     }
     else if (size < 1000000)
     {
-        return QObject::tr("%1 Ko").arg(QString::number((double)size/1000.0, 'f', 1));
+        return QObject::tr("%1 KB").arg(QString::number((double)size/1000.0, 'f', 1));
     }
     else if (size < 1000000000)
     {
-        return QObject::tr("%1 Mo").arg(QString::number((double)size/1000000.0, 'f', 1));
+        return QObject::tr("%1 MB").arg(QString::number((double)size/1000000.0, 'f', 1));
     }
     else
     {
-        return QObject::tr("%1 Go").arg(QString::number((double)size/1000000000.0, 'f', 1));
+        return QObject::tr("%1 GB").arg(QString::number((double)size/1000000000.0, 'f', 1));
     }
 }
