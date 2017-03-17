3a43fe32ad10143248c36f408d27b559a303db8e
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2017-03-17 11:35:23 +0100

    Add 32-samples loops at end of wavs
    Was missing in 09eb94b6ef47051051d1a3d1f73e004cee556327

--- dusoundfile/dusample.cpp
+++ dusoundfile/dusample.cpp
@@ -299,12 +299,12 @@ DuSamplePtr DuSample::fromWav(QFile *input)
     }
 
     QByteArray rawData = wavFileData.mid(dataKeywordIndex + 4 + 4, static_cast<int>(wavSize)); // +4 for "data", +4 for size
-    rawData.append(QByteArray(16, 0x00)); // Append 8 half words to loop over at the end of the sample
+    rawData.append(QByteArray(64, 0x00)); // Append 32 half words to loop over at the end of the sample
 
     DuSamplePtr sample(new DuSample);
 
     sample->setData(rawData);
-    sample->setLoopStart(rawData.size() - 16);
+    sample->setLoopStart(rawData.size() - 64);
     sample->setLoopEnd(rawData.size() - 2);
 
     return sample;
