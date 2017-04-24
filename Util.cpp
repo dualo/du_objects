d328cab9608d18577a391005c8987a9eb6affe52
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2017-04-24 16:46:37 +0200

    [Util] Fix size handling (use 1024 instead of 1000)

--- Util.cpp
+++ Util.cpp
@@ -4,21 +4,21 @@
 
 QString Util::sizeToString(quint64 size)
 {
-    if (size < 1000)
+    if (size < 1024)
     {
         return QObject::tr("%1 Bytes").arg(size);
     }
-    else if (size < 1000000)
+    else if (size < 1024*1024)
     {
-        return QObject::tr("%1 KB").arg(QString::number(static_cast<double>(size)/1000.0, 'f', 1));
+        return QObject::tr("%1 KB").arg(QString::number(static_cast<double>(size)/1024.0, 'f', 1));
     }
-    else if (size < 1000000000)
+    else if (size < 1024*1024*1024)
     {
-        return QObject::tr("%1 MB").arg(QString::number(static_cast<double>(size)/1000000.0, 'f', 1));
+        return QObject::tr("%1 MB").arg(QString::number(static_cast<double>(size)/(1024.0*1024.0), 'f', 1));
     }
     else
     {
-        return QObject::tr("%1 GB").arg(QString::number(static_cast<double>(size)/1000000000.0, 'f', 1));
+        return QObject::tr("%1 GB").arg(QString::number(static_cast<double>(size)/(1024.0*1024.0*1024.0), 'f', 1));
     }
 }
 
