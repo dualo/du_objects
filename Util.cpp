3de55ab193b32af1e36fb7407ed23a292d3a667a
Nicolas Kniebihler <nicolas.kniebihler@dualo.org>
2017-04-26 12:11:36 +0200

    [Refacto] Add spaces between operators

--- Util.cpp
+++ Util.cpp
@@ -8,17 +8,17 @@ QString Util::sizeToString(quint64 size)
     {
         return QObject::tr("%1 Bytes").arg(size);
     }
-    else if (size < 1024*1024)
+    else if (size < 1024 * 1024)
     {
-        return QObject::tr("%1 KB").arg(QString::number(static_cast<double>(size)/1024.0, 'f', 1));
+        return QObject::tr("%1 KB").arg(QString::number(static_cast<double>(size) / 1024.0, 'f', 1));
     }
-    else if (size < 1024*1024*1024)
+    else if (size < 1024 * 1024 * 1024)
     {
-        return QObject::tr("%1 MB").arg(QString::number(static_cast<double>(size)/(1024.0*1024.0), 'f', 1));
+        return QObject::tr("%1 MB").arg(QString::number(static_cast<double>(size) / (1024.0 * 1024.0), 'f', 1));
     }
     else
     {
-        return QObject::tr("%1 GB").arg(QString::number(static_cast<double>(size)/(1024.0*1024.0*1024.0), 'f', 1));
+        return QObject::tr("%1 GB").arg(QString::number(static_cast<double>(size) / (1024.0 * 1024.0 * 1024.0), 'f', 1));
     }
 }
 
@@ -34,14 +34,14 @@ QString Util::timeToString(qint64 msTime)
     }
     else if (msTime < 60000)
     {
-        return QObject::tr("%1 sec").arg(QString::number(static_cast<double>(msTime)/1000.0, 'f', 1));
+        return QObject::tr("%1 sec").arg(QString::number(static_cast<double>(msTime) / 1000.0, 'f', 1));
     }
     else if (msTime < 3600000)
     {
-        return QObject::tr("%1 min").arg(QString::number(static_cast<double>(msTime)/60000.0, 'f', 1));
+        return QObject::tr("%1 min").arg(QString::number(static_cast<double>(msTime) / 60000.0, 'f', 1));
     }
     else
     {
-        return QObject::tr("%1 hrs").arg(QString::number(static_cast<double>(msTime)/3600000.0, 'f', 1));
+        return QObject::tr("%1 hrs").arg(QString::number(static_cast<double>(msTime) / 3600000.0, 'f', 1));
     }
 }
